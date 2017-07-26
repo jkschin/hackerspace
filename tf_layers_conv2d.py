ebc6ecf3889013d158e9900f87cccdb088fee2c4
Samuel Chin <samuelchin91@gmail.com>
2017-07-26 15:32:22 +0700

    tf_layers_conv2d
    Verified that using get_shape or shape as arguments to conv2d works.

--- tf_layers_conv2d.py
+++ tf_layers_conv2d.py
@@ -1,12 +1,16 @@
 import tensorflow as tf
 
+b = tf.random_uniform([10, 10, 10, 10])
+print b.shape
+print b.get_shape()[0]
 params = {
     'inputs': tf.random_uniform([1, 3, 3, 3]),
-    'filters': 1,
+    'filters': b.get_shape()[0],
     'kernel_size': [3, 3],
     }
 
 a = tf.layers.conv2d(**params)
+print a.shape
 with tf.Session() as sess:
   sess.run(tf.global_variables_initializer())
   print sess.run(a)
