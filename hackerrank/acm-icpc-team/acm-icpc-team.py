ae37deb96be10f38d19a31a50f2fb4843ef9be2e
Samuel Chin <samuelchin91@gmail.com>
2018-11-24 21:38:37 +0800

    Updated code.

--- hackerrank/acm-icpc-team/acm-icpc-team.py
+++ hackerrank/acm-icpc-team/acm-icpc-team.py
@@ -1,26 +1,20 @@
+# There was a timeout on the initial code. That's because I used a different way
+# to get the counts of 1s in the binary string. Turns out that it is
+# ridiculously inefficient and a few methods are explored in test_speed.py
 def acmTeam(topic):
     dic = {}
     for i in xrange(len(topic)):
         a = int(topic[i], 2)
         for j in xrange(i+1, len(topic)):
             b = int(topic[j], 2)
-            # c = sum([int(z) for z in list(bin(a | b)[2:])])
-            c = a | b
+            # This was the line that TLE.
+            c = bin(a | b)[2:].count('1')
             if c not in dic:
                 dic[c] = 1
             else:
                 dic[c] += 1
-
-    new_dic = {}
-    for key in dic.keys():
-        val = dic[key]
-        new_key = sum([int(z) for z in list(bin(key)[2:])])
-        if new_key not in new_dic:
-            new_dic[new_key] = val
-        else:
-            new_dic[new_key] += val
-    max_subs = max(new_dic.keys())
-    max_teams = new_dic[max_subs]
+    max_subs = max(dic.keys())
+    max_teams = dic[max_subs]
     return [max_subs, max_teams]
 
 f = open('acm-icpc-team.in').read().split('\n')[:-1]
